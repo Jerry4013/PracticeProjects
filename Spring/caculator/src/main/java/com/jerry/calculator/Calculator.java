35419c1ac47507100b0baccda57862df002382fa
I505998 <jingchao.zhang@mail.concordia.ca>
2019-04-06 16:44:10 -0400

    calculator

---
+++ Spring/caculator/src/main/java/com/jerry/calculator/Calculator.java
@@ -0,0 +1,13 @@
+package com.jerry.calculator;
+
+import lombok.Data;
+
+@Data
+public class Calculator {
+
+    private String operator;
+    private double operand1;
+    private double operand2;
+    private double result;
+
+}
